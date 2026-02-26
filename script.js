e15405a6d9e60b6b7413d7511cc3ea30d94bb735
xkarimass <kacpercwiekala212@gmail.com>
2026-02-26 12:03:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,66 +1,41 @@
-const items = [
-  {
-    id: 1,
-    title: "Death Note",
-    type: "anime",
-    ratingSum: 45,
-    ratingCount: 5
-  },
-  {
-    id: 2,
-    title: "Cyberpunk 2077",
-    type: "game",
-    ratingSum: 40,
-    ratingCount: 5
-  },{
-    id: 3,
-    title: "Femboy Futa House",
-    type: "game",
-    ratingSum: 50,
-    ratingCount: 5
-  },{
-    id: 4,
-    title: "Miside",
-    type: "game",
-    ratingSum: 30,
-    ratingCount: 5
-  },{
-    id: 5,
-    title: "Garden warefare 2",
-    type: "game",
-    ratingSum: 25,
-    ratingCount: 5
-  }
+// lista anime/gier
+let items = [
+  { title: "Death Note", type: "anime" },
+  { title: "Attack on Titan", type: "anime" },
+  { title: "Cyberpunk 2077", type: "game" },
+  { title: "The Witcher 3", type: "game" }
 ];
 
-const list = document.getElementById("list");
+let searchInput = document.getElementById("search");
+let suggestions = document.getElementById("suggestions");
 
-function render() {
-  list.innerHTML = "";
+searchInput.addEventListener("input", () => {
+  let query = searchInput.value.toLowerCase();
+  suggestions.innerHTML = "";
 
-  items.forEach(item => {
-    const avg = (item.ratingSum / item.ratingCount).toFixed(1);
+  if (!query) {
+    suggestions.style.display = "none";
+    return;
+  }
 
-    const div = document.createElement("div");
-    div.className = "card";
+  let matches = items.filter(item => item.title.toLowerCase().includes(query));
 
-    div.innerHTML = `
-      <h2>${item.title}</h2>
-      <p>Średnia: ⭐ ${avg}</p>
-      <div class="rating">
-        ${[1,2,3,4,5,6,7,8,9,10].map(n =>
-          `<span onclick="rate(${item.id}, ${n})">⭐</span>`
-        ).join("")}
-      </div>
-    `;
-
-    list.appendChild(div);
+  matches.forEach(item => {
+    let div = document.createElement("div");
+    div.textContent = item.title + " (" + item.type + ")";
+    div.addEventListener("click", () => {
+      window.location.href = "details.html? title=" + encodeURIComponent(item.title);
+    });
+    suggestions.appendChild(div);
   });
-}
 
-function rate(id, value) {
-  const item = items.find(i => i.id === id);
-  item.ratingSum += value;
-  item.ratingCount++;
-  render();
+  suggestions.style.display = matches.length ? "block" : "none";
+});
+searchInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        let value = searchInput.value.trim();
+        if (value){
+        window.location.href = "details.html?title=" + encodeURIComponent(value);
+    }
 }
+});
